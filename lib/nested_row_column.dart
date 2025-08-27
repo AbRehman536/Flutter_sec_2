cc3355c5c2bee01e2cac18b06e7d6f05e2058e22
Abdullah <abdullahrehman1019@gmail.com>
2025-08-27 10:41:37 -0600

    Nested_Row_Column done

---
+++ lib/nested_row_column.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class NestedRowColumn extends StatelessWidget {
+  const NestedRowColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Nested Row Column"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20,),
+        Text("Lorem Ipsum",style: TextStyle(fontSize: 30),),
+          SizedBox(height: 20,),
+        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+          SizedBox(height: 20,),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          Icon(Icons.favorite),
+          Icon(Icons.comment),
+          Icon(Icons.bookmark),
+          Icon(Icons.person),
+        ],),
+          SizedBox(height: 20,),
+          Text("Lorem Ipsum",style: TextStyle(fontSize: 30),),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+            Icon(Icons.email),
+            Icon(Icons.access_alarm),
+          ],)
+
+      ],),
+    );
+  }
+}
